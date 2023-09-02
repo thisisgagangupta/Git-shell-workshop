2a05e372a6f01b262fbf9860f79852098736cf32
HarshitaKamani <harshitakamani03@gmail.com>
2023-09-03 02:06:56 +0530

    task_4

--- script.sh
+++ script.sh
@@ -1,25 +1,30 @@
-# Task 2:
 for var in "$@"
-do 
-	echo "Running $var on test cases"
-	for i in $(seq 3)
-	do
-		echo "Running test case $i:"
-		cat input$i.txt | python3 $var > user_output$i.txt 
-		if [[ $(diff user_output$i.txt output$i.txt) ]]
-		then
-		echo "Test case $i failed"
-		echo "Expected output" 
-		cat output$i.txt 
-		echo "User output: " 
-		cat user_output$i.txt 
-		else
-		echo "Test case $i passed"
-		fi
-		echo "--------------------------------------------------------------"
-	done
-	echo "================================================================="
+do
+    echo "Running $var on test cases"
+    for i in $(seq 3) 
+    do
+        echo "Running test case $i:"
+       
+        cat input$i.txt | python3 $var > user_output$i.txt 2>error_files$i.txt
+        if [ $? -eq 0 ]
+        then
+        	echo "Code ran without run-time error"
+       		if [[ $(diff user_output$i.txt output$i.txt) ]] 
+        	then
+            		echo "Test case $i failed"
+            		echo "Expected output"
+            		cat output$i.txt
+            		echo "User output: "
+            		cat user_output$i.txt
+        	else
+            		echo "Test case $i passed"
+        	fi
+        else
+        	echo "Code has run-time errors"
+        	cat error_files$i.txt
+        fi
+       
+        echo "--------------------------------------------------------------"
+    done
+    echo "================================================================="
 done
-
-
-
