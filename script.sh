c4d62f9e98bf6adcb5dad37e5bdb03439c013156
HarshitaKamani <harshitakamani03@gmail.com>
2023-09-03 00:25:11 +0530

    1st task

--- script.sh
+++ script.sh
@@ -5,3 +5,15 @@
 # Run the python file, using input1.txt as stdin, and write the output to a file user_output1.txt 
 # Display user_output1.txt to the user as user output, and the file output1.txt as the expected output 
 # Once you have tested out the script and verified it's working, commit it to a branch: <your_name>
+
+language=$1
+echo "the language used is $language"
+
+gcc $2
+cat input1.txt | ./a.out > user_output1.txt
+echo "User output: "
+cat user_output1.txt
+echo "Expected output: "
+cat output1.txt
+
+
