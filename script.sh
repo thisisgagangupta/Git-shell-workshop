ef650dbebe97a7a1f55496b25e54032782e93f03
HarshitaKamani <harshitakamani03@gmail.com>
2023-09-03 02:24:28 +0530

    task_5

--- script.sh
+++ script.sh
@@ -1,30 +1,40 @@
-for var in "$@"
-do
-    echo "Running $var on test cases"
-    for i in $(seq 3) 
-    do
-        echo "Running test case $i:"
+language=$1
+echo "The language passed as argument is $language"
+if [[ language=="python" ]]
+then
+	echo "Language verified"
+	for var in "${@:2}"
+	do
+    		echo "Running $var on test cases"
+    		for i in $(seq 3) 
+    		do
+        		echo "Running test case $i:"
        
-        cat input$i.txt | python3 $var > user_output$i.txt 2>error_files$i.txt
-        if [ $? -eq 0 ]
-        then
-        	echo "Code ran without run-time error"
-       		if [[ $(diff user_output$i.txt output$i.txt) ]] 
-        	then
-            		echo "Test case $i failed"
-            		echo "Expected output"
-            		cat output$i.txt
-            		echo "User output: "
-            		cat user_output$i.txt
-        	else
-            		echo "Test case $i passed"
-        	fi
-        else
-        	echo "Code has run-time errors"
-        	cat error_files$i.txt
-        fi
+        		cat input$i.txt | python3 $var > user_output$i.txt 2>error_files$i.txt
+        		if [ $? -eq 0 ]
+        		then
+        			echo "Code ran without run-time error"
+       				if [[ $(diff user_output$i.txt output$i.txt) ]] 
+        			then
+            				echo "Test case $i failed"
+            				echo "Expected output"
+            				cat output$i.txt
+            				echo "User output: "
+            				cat user_output$i.txt
+        			else
+            				echo "Test case $i passed"
+        			fi
+        		else
+        			echo "Code has run-time errors"
+        			cat error_files$i.txt
+        		fi
+        		echo "--------------------------------------------------------------"
+    		done
+    		echo "================================================================="
+	done
+else
+	echo "Invalid language"
+fi
+
+
        
-        echo "--------------------------------------------------------------"
-    done
-    echo "================================================================="
-done
