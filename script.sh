3f382affb8677a3e03b954e5a89cc215c2652e78
HarshitaKamani <harshitakamani03@gmail.com>
2023-09-03 01:31:45 +0530

    task_3

--- script.sh
+++ script.sh
@@ -5,11 +5,17 @@ do
 	for i in $(seq 3)
 	do
 		echo "Running test case $i:"
-		cat input$i.txt | gcc $var > user_output$i.txt 
+		cat input$i.txt | python3 $var > user_output$i.txt 
+		if [[ $(diff user_output$i.txt output$i.txt) ]]
+		then
+		echo "Test case $i failed"
+		echo "Expected output" 
+		cat output$i.txt 
 		echo "User output: " 
 		cat user_output$i.txt 
-		echo "Expected output: " 
-		cat output$i.txt 
+		else
+		echo "Test case $i passed"
+		fi
 		echo "--------------------------------------------------------------"
 	done
 	echo "================================================================="
