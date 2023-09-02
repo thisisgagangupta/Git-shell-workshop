1ddeb1694fc13149fd93194517350c8a6af436fb
HarshitaKamani <harshitakamani03@gmail.com>
2023-09-03 01:04:07 +0530

    task_2

--- script.sh
+++ script.sh
@@ -1,19 +1,19 @@
-# Task 1: 
-# The first argument provided from cmd must be the language you are running it in
-# Just plainly print the first argument that was provided 
-# The second argument is the name of the python/C file that must be run 
-# Run the python file, using input1.txt as stdin, and write the output to a file user_output1.txt 
-# Display user_output1.txt to the user as user output, and the file output1.txt as the expected output 
-# Once you have tested out the script and verified it's working, commit it to a branch: <your_name>
+# Task 2:
+for var in "$@"
+do 
+	echo "Running $var on test cases"
+	for i in $(seq 3)
+	do
+		echo "Running test case $i:"
+		cat input$i.txt | gcc $var > user_output$i.txt 
+		echo "User output: " 
+		cat user_output$i.txt 
+		echo "Expected output: " 
+		cat output$i.txt 
+		echo "--------------------------------------------------------------"
+	done
+	echo "================================================================="
+done
 
-language=$1
-echo "the language used is $language"
-
-gcc $2
-cat input1.txt | ./a.out > user_output1.txt
-echo "User output: "
-cat user_output1.txt
-echo "Expected output: "
-cat output1.txt
 
 
